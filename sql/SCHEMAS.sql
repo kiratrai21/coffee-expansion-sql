fc781a57e1357bc28c91a7a9e4fd304d9a8f37ea
kiratrai21 <123400210+kiratrai21@users.noreply.github.com>
2026-01-08 22:49:37 +0530

    Update SCHEMAS.sql

--- sql/SCHEMAS.sql
+++ sql/SCHEMAS.sql
@@ -1,15 +1,9 @@
--- Monday Coffee SCHEMAS
+-- SCHEMAS
 
 DROP TABLE IF EXISTS sales;
 DROP TABLE IF EXISTS customers;
 DROP TABLE IF EXISTS products;
 DROP TABLE IF EXISTS city;
-
--- Import Rules
--- 1st import to city
--- 2nd import to products
--- 3rd import to customers
--- 4th import to sales
 
 
 CREATE TABLE city
